4ac1e2b55032310f0daf255110cfc414d36ef0bf
M1lken01 <xmilken01x@gmail.com>
2022-09-14 22:55:05 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# csgo-case-chances
- 
+# CSGO case chances
+
+Simulates opening a lot of CSGO cases at once.
+
+## Getting Started
+
+### Installing
+
+* Run this command:
+
+```bash
+pip install -r setup.txt
+```
+
+* Edit the cfg.json settings
+
+### Executing program
+
+* Open main.py
+
+## Authors
+
+[@M1lken01](https://github.com/M1lken01)
--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import json
 import random
 import time
 import colored
+from os import system
 
 data = {"tries": 0, "blue": 0, "purple": 0, "pink": 0, "red": 0, "gold": 0}
-cfg = {"tries": 9999, "delay": 0.05, "auto": False , "reset": False }
-
+cfg = {"no-save": False, "cases": 9999, "delay": 0.05, "auto": False , "reset": False, "gold-stop": False}
+dark = colored.fg(232)
+light = colored.fg(231)
+stop = False
 
 def main():
+    system("title csgo-case-chances")
     global data, cfg
     if not cfg["reset"]:
         with open("db.json", "r") as db:
@@ -27,9 +31,10 @@ def caseLoop():
     if cfg["reset"]:
         global data
         data = {"tries": 0, "blue": 0, "purple": 0, "pink": 0, "red": 0, "gold": 0}
+    global stop
     m = 0
     c = colored.bg(235)
-    while m < cfg["tries"]:
+    while m < cfg["cases"] and stop == False:
         n = random.randint(0, 9999999)
         m += 1
         t = "none"
@@ -37,7 +42,9 @@ def caseLoop():
         if n <= 25575:
             data["gold"] += 1
             t = "Gold"
-            c = colored.bg(136)
+            c = colored.bg(136) + dark
+            if cfg["gold-stop"]:
+                stop = True
         elif n <= 89514:
             data["red"] += 1
             t = "Red"
@@ -54,7 +61,7 @@ def caseLoop():
             data["blue"] += 1
             t = "Blue"
             c = colored.bg(18)
-        print(c + f'| {m:07} | {n:07} | {t:7} |' + colored.bg(0))
+        print(c + f'| {m:07} | {n:07} | {t:7} |' + colored.bg(0) + light)
         time.sleep(cfg["delay"])
     saveToDb()
 
@@ -65,6 +72,9 @@ def saveToDb():
         db.close()
     d = data
     getData = d
+    if cfg["no-save"]:
+        info(getData)
+        return
     with open("db.json", "w") as db:
         json.dump(d, db)
         db.close()
@@ -78,13 +88,16 @@ Values:
 {colored.bg(54)} Purples : {db["purple"]:07}{colored.bg(234)}
 {colored.bg(161)} Pinks   : {db["pink"]:07}{colored.bg(234)}
 {colored.bg(124)} Reds    : {db["red"]:07}{colored.bg(234)}
-{colored.bg(136)} Golds   : {db["gold"]:07}{colored.bg(234)}
+{colored.bg(136)}{dark} Golds   : {db["gold"]:07}{colored.bg(234)}{light}
 Info:
  Cases   : {db["tries"]:07}{colored.bg(0)}
 ---------------------------------""")
     if not cfg["auto"]:
         input('Enter to continue.\n')
-    caseLoop()
+    if stop:
+        input("Enter to exit.")
+    else:
+        caseLoop()
 
 
 if __name__ == '__main__':
